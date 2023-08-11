3c93cb7fdf32ddefcbf5ad28cf5f2c059e036f3d
Virtuoso-Web <virtue_dollars.0p@icloud.com>
2023-08-11 11:59:04 +0200

    Upload

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,9 @@
 
 import Link from "next/link";
 
-import { motion } from "framer-motion";
+import { useState } from "react";
+
+import { motion, AnimatePresence } from "framer-motion";
 
 import { BsArrowLeft } from "react-icons/bs";
 import { BsArrowRight } from "react-icons/bs";
@@ -11,34 +13,103 @@ import { MdOutlineWavingHand } from "react-icons/md";
 
 import "@/app/style.scss";
 
+const products = [
+    {
+        key: 1,
+        title: "Title 1",
+        description: "Description 1",
+        image: "Image 1",
+    },
+    {
+        key: 2,
+        title: "Title 2",
+        description: "Description 2",
+        image: "Image 2",
+    },
+    {
+        key: 3,
+        title: "Title 3",
+        description: "Description 3",
+        image: "Image 3",
+    },
+    {
+        key: 4,
+        title: "Title 4",
+        description: "Description 4",
+        image: "Image 4",
+    },
+    {
+        key: 5,
+        title: "Title 5",
+        description: "Description 5",
+        image: "Image 5",
+    },
+];
+
+const switches = {
+    initial: (dir) => {
+        return { opacity: 0, x: dir > 0 ? "10vw" : "-10vw" };
+    },
+    animate: { opacity: 1, x: 0, transition: { ease: "linear", delay: 0.2, duration: 0.2 } },
+    exit: (dir) => {
+        return { opacity: 0, x: dir > 0 ? "-10vw" : "10vw", transition: { ease: "linear", duration: 0.2 } };
+    },
+};
+
 const scales = {
     hidden: { scale: 0 },
     visible: { scale: 1, transition: { type: "spring", duration: 0.4 } },
 };
 
 export default function Page() {
+    const [dir, setDir] = useState(0);
+    const [slide, setSlide] = useState(0);
+
+    const prev = () => {
+        setDir(-1);
+        setSlide((slide) => (slide === 0 ? products.length - 1 : slide - 1));
+    };
+
+    const next = () => {
+        setDir(1);
+        setSlide((slide) => (slide === products.length - 1 ? 0 : slide + 1));
+    };
+
     return (
         <main className="home-page">
             <section className="hero-section">
-                <img src="/images/background.webp" alt="Hintergrund" className="bg-image" />
+                <img src="/images/bg-concrete.webp" alt="Hintergrund" className="bg-image" />
                 <div className="section-container">
-                    <div className="flex-grid">
-                        <div className="title-box">
-                            <motion.h1 initial={{ opacity: 0, y: "5vh" }} animate={{ opacity: 1, y: 0, transition: { type: "spring", duration: 0.4 } }} className="title">
-                                EXPERTEN IN SACHEN BAUMESSTECHNIK
-                            </motion.h1>
+                    <div className="hero-slider">
+                        <div className="icon-holder-prev" onClick={prev}>
+                            <BsArrowLeft className="icon" />
                         </div>
                         <div className="text-box">
-                            <motion.p initial={{ opacity: 0, y: "5vh" }} animate={{ opacity: 1, y: 0, transition: { type: "spring", duration: 0.4, delay: 0.2 } }} className="intro">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos mollitia nam voluptas dolor vel! Magni voluptatum cum perferendis illum corrupti. Lorem, ipsum dolor sit amet consectetur adipisicing elit?
-                            </motion.p>
-                            <motion.div initial={{ opacity: 0, y: "5vh" }} animate={{ opacity: 1, y: 0, transition: { type: "spring", duration: 0.4, delay: 0.4 } }} className="motion-div">
-                                <Link href={"/shop"} prefetch={false} className="link">
-                                    Produkte entdecken
-                                </Link>
-                            </motion.div>
+                            <div className="flex-box">
+                                <AnimatePresence initial={false} custom={dir}>
+                                    <motion.h1 initial={"initial"} animate={"animate"} exit={"exit"} variants={switches} custom={dir} key={slide} className="title">
+                                        {products[slide].title}
+                                    </motion.h1>
+                                </AnimatePresence>
+                            </div>
+                            <div className="flex-box">
+                                <AnimatePresence initial={false} custom={dir}>
+                                    <motion.p initial={"initial"} animate={"animate"} exit={"exit"} variants={switches} custom={dir} key={slide} className="description">
+                                        {products[slide].description}
+                                    </motion.p>
+                                </AnimatePresence>
+                            </div>
+                        </div>
+                        <div className="image-box">
+                            <div className="image-holder"></div>
+                        </div>
+                        <div className="icon-holder-next" onClick={next}>
+                            <BsArrowRight className="icon" />
                         </div>
                     </div>
+                    <Link href={"/shop"} className="link">
+                        Zum Shop
+                    </Link>
                 </div>
             </section>
 
